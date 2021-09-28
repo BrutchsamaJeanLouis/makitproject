a8792bd5db18206fd9a4f78e93857f7edcef713d
BrutchsamaJeanLouis <brutchsama14@gmail.com>
2021-09-28 16:58:59 +0100

    root reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,6 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import  ThunkMiddleware from 'redux-thunk';
 import counterReducer from '../features/counter/counterSlice';
 
+const rootReducer = {
+  userID: 'imported Reducer Goes Here',
+  username: 'Imported Reducer Goes here',
+  
+
+
+}
+
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
@@ -11,3 +20,15 @@ export const store = configureStore({
   devTools:true
 
 });
+
+
+// using thunk to fetch store initial state
+// const exampleThunkFunction = (dispatch, getState) => {
+//   const stateBefore = getState()
+//   console.log(`Counter before: ${stateBefore.counter}`)
+//   dispatch(increment())
+//   const stateAfter = getState()
+//   console.log(`Counter after: ${stateAfter.counter}`)
+// }
+
+// store.dispatch(exampleThunkFunction)
